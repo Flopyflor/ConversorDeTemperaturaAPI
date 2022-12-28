1d8c43bd7dba4abb4a16db965b5bb7fb5a3a77c9
Florencia Allami <allami.florencia@gmail.com>
2022-12-28 09:54:29 -0300

    initial commit. Spring boot set up

---
+++ src/test/java/com/allamiflorencia/ConversorDeTemperatura/ConversorDeTemperaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allamiflorencia.ConversorDeTemperatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversorDeTemperaturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
